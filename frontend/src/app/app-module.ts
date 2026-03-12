ebf50aaec64d2a6455e34edfbf0de1f52fe9cf25
Alejandro Alvarez <alvarez.h.alejo@gmail.com>
2026-03-12 11:14:17 -0400

    feat: agregar pantalla home

--- frontend/src/app/app-module.ts
+++ frontend/src/app/app-module.ts
@@ -1,12 +1,13 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
-import { HttpClientModule } from '@angular/common/http';
+import { Home } from './features/home/home';
 
 @NgModule({
-  declarations: [App],
+  declarations: [App, Home],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
--- frontend/src/app/app-routing-module.ts
+++ frontend/src/app/app-routing-module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Home } from './features/home/home';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'register', pathMatch: 'full' },
+  { path: '', component: Home },
   
   {
     path: '',
