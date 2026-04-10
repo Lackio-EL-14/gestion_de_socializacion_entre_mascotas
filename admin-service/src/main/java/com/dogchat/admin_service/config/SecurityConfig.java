f80eecdb32e7ed250af62e30cb63c0c8b55c1d2a
Metzar <metzariel-14@localhost.localdomain>
2026-04-10 11:58:29 -0400

    feat:docmuentacion lista

---
+++ admin-service/src/main/java/com/dogchat/admin_service/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.dogchat.admin_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            
+            .cors(cors -> cors.configure(http))
+            
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().authenticated()
+            )
+            
+            .oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(jwt -> jwt.decoder(jwtDecoder()))
+            );
+
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        String secret = "super-secreto-dogchat-2026-seguro"; 
+        
+        SecretKey secretKey = new SecretKeySpec(secret.getBytes(), "HmacSHA256");
+        return NimbusJwtDecoder.withSecretKey(secretKey).build();
+    }
+}
