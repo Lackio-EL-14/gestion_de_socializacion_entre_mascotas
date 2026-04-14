d93ac40f389e59316bbdb537d2df73e277d419b0
atalasluis <atalasturey@gmail.com>
2026-04-14 03:36:04 -0400

    feat: implemantacion de patch para la moderacion de publicaciones

--- backend/src/modules/publications/publications.module.ts
+++ backend/src/modules/publications/publications.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Publication } from './entities/publications.entity';
+import { PublicationsService } from './service/publications.service';
+import { PublicationsController } from './controller/publications.controller';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Publication])],
+  controllers: [PublicationsController],
+  providers: [PublicationsService],
+})
 export class PublicationsModule {}
