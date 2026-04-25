6e0c5def2da80a07d78263b9222998abb1203cf8
AlejoHuay <alvarez.h.alejo@gmail.com>
2026-04-25 12:55:52 -0400

    feat: se implemento match entre mascotas desde pantalla feed-home con dinamica huesito y next

--- frontend/src/app/features/feed/feed-module.ts
+++ frontend/src/app/features/feed/feed-module.ts
@@ -6,10 +6,11 @@ import { FeedRoutingModule } from './feed-routing-module';
 import { FeedHome } from './feed-home/feed-home';
 import { Filtros } from './feed-filtros/filtros';
 import { SharedModule } from '../../shared/shared-module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [FeedHome, Filtros],
-  imports: [CommonModule, FeedRoutingModule, TranslateModule, SharedModule],
+  imports: [CommonModule, FeedRoutingModule, TranslateModule, SharedModule, FormsModule],
 })
 export class FeedModule {}
   
