14529f9bd5c2f6999f17834a078b69f50464cabe
AlejoHuay <alvarez.h.alejo@gmail.com>
2026-05-05 17:23:57 -0400

    feat: agrego guards por rol para controlar el bug de cambio de dashboards por url

--- frontend/src/app/app-routing-module.ts
+++ frontend/src/app/app-routing-module.ts
@@ -6,13 +6,44 @@ import { DashboardEditOwner } from './features/dashboards/dashboard-edit_owner/d
 import { DashboardAdmin } from './features/dashboards/dashboard-admin/dashboard-admin';
 import { DashboardWorkerComponent } from './features/dashboards/dashboard-worker/dashboard-worker';
 import { AdminContentPublicationsComponent } from './features/dashboards/dashboard-admin/content-management/content-management';
+import { roleGuard } from './core/guards/role.guard';
 
 const routes: Routes = [
-  { path: 'dashboard-worker', component: DashboardWorkerComponent },
+  /*{ path: 'dashboard-worker', component: DashboardWorkerComponent },
   { path: 'dashboard-owner', component: DashboardOwner },
   { path: 'dashboard-admin', component: DashboardAdmin },
   { path: 'admin/content', component: AdminContentPublicationsComponent },
-  { path: 'dashboard-edit-owner', component: DashboardEditOwner },
+  { path: 'dashboard-edit-owner', component: DashboardEditOwner },*/
+  { 
+    path: 'dashboard-worker', 
+    component: DashboardWorkerComponent,
+    canActivate: [roleGuard],
+    data: { roles: [3] }
+  },
+  { 
+    path: 'dashboard-owner', 
+    component: DashboardOwner,
+    canActivate: [roleGuard],
+    data: { roles: [1] }
+  },
+  { 
+    path: 'dashboard-admin', 
+    component: DashboardAdmin,
+    canActivate: [roleGuard],
+    data: { roles: [2] }
+  },
+  { 
+    path: 'admin/content', 
+    component: AdminContentPublicationsComponent,
+    canActivate: [roleGuard],
+    data: { roles: [2] }
+  },
+  { 
+    path: 'dashboard-edit-owner', 
+    component: DashboardEditOwner,
+    canActivate: [roleGuard],
+    data: { roles: [1] }
+  },
   {
     path: 'publications',
     loadChildren: () =>
